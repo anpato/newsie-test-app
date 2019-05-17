644f28b0da27ce549ee04455af53de1db3e530d9
Andre Pato <anpato1994@gmail.com>
2019-05-17 16:41:48 -0400

    initial commit

---
+++ src/components/Trending/Trending.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Trending extends Component {
+    constructor() {
+        super();
+        this.state = {
+            trending: []
+        }
+    }
+
+    async componentDidMount() {
+        const {api_key} = this.props
+        try {
+            const url = await fetch(`https://newsapi.org/v2/sources?country=us&apiKey=${api_key}`);
+            const resp = await url.json();
+            return this.setState({trending:resp.sources})
+        } catch (error) {
+            throw error
+        }
+    }
+    
+    handleclick=(e) => {
+        const name = e.target.getAttribute('name')
+        console.log(name)
+        return e.target.value
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.trending.map((source,index) => {
+                    console.log(source.id)
+                    return <li key={index}  name={source.id} onClick={this.handleclick}>{source.name}</li>
+                })}
+            </div>
+        );
+    }
+}
+
+export default Trending;
