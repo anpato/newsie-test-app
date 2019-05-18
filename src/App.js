f82080cbef7326a8ae7b0c10cf34b1d867480da5
Andre Pato <anpato1994@gmail.com>
2019-05-18 10:32:27 -0400

    added sass and styles folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/App.scss';
 import SignIn from './components/SignIn/SignIn'
 import { Switch, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard/Dashboard';
