b08fbceff66db241c0e682e2ec37f4465873ac21
larsthrasher <thrasherlars@gmail.com>
2019-05-18 16:36:00 -0400

    iphone sign-in page edits

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Redirect, Route } from 'react-router-dom'
 import Dashboard from '../Dashboard/Dashboard';
+import './SignIn.css'
+
 class SignIn extends Component {
     constructor(props){
         super(props);
@@ -25,19 +27,20 @@ class SignIn extends Component {
 
     render() {
         const { username,password,isAuthenticated  } = this.state
-        
+
         if(isAuthenticated === true){
             return <Redirect to='/dashboard'/>
         }
-        
+
         return (
             <div className="signIn-form">
-                <form onChange={this.handleFormChange} onSubmit={this.handleSubmit}>
-                    <label htmlFor="username">Username</label>
-                        <input type="text" name="username" defaultValue={username}/>
-                    <label>Password</label>
-                        <input htmlFor="username" type="password" name="password" defaultValue={password}/>
-                    <button type="submit">Sign In</button>
+                <form className="forma" onChange={this.handleFormChange} onSubmit={this.handleSubmit}>
+                    <label htmlFor="username"></label>
+                    <br/>
+                        <input type="text" name="username" placeholder='username' defaultValue={username}/>
+                        <input htmlFor="username" type="password" name="password" placeholder='password' defaultValue={password}/>
+                        <br/>
+                    <button className="subButton" type="submit">Sign In</button>
                 </form>
                 <Route exact path='/dashboard' component={Dashboard}/>
             </div>
