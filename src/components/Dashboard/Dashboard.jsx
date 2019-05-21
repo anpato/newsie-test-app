aefd0864c5a6c2cdf138bf4de5f4dd4c8291d037
Loriana Hines <lorianahines@gmail.com>
2019-05-21 14:41:37 -0400

    styled the article component of the dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -21,17 +21,21 @@ class Dashboard extends Component {
         const {articles} = this.state
         const allArticles =  articles ? articles.map((article,index) =>{
             return <div className='article-container' key={index}>
-                        <img src={article.urlToImage} alt='Article' />
-                        <h2>{article.source.name}</h2>
-                        <h1>{article.title}</h1>
-                        <h4>{moment(article.publishedAt).format('MM/DD HH:mm A')}</h4>
-                        <p>{article.description}</p>
+                        <h3>Business</h3>
+                        <div className='article-info'>
+                            <img src={article.urlToImage} alt='Article' />
+                            <h2>{article.source.name}</h2>
+                            <h1>{article.title}</h1>
+                            <h4>{moment(article.publishedAt).format('MM/DD HH:mm A')}</h4>
+                            <p>{article.description}</p>
+                        </div>
                     </div>
         }) : () => <h1>Articles loading...</h1> 
         return (
-            <div>
+            <div className='dashboard-container'> 
                 <h1>n</h1>
-                <Trending/>
+                {/* <Trending/> */}
+
                 {allArticles}
             </div>
         );
