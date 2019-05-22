0ccd9eeddee3c19c853741fb59c7fb75e9b37337
Andre Pato <anpato1994@gmail.com>
2019-05-22 15:22:31 -0400

    Updates

---
+++ precache-manifest.73e8f88ac904f341102f2e0f9f4c1eb1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46971e4ff70630d7ded504936b2c155f",
+    "url": "/newsie/index.html"
+  },
+  {
+    "revision": "d0716139e3036c6c4d38",
+    "url": "/newsie/static/css/main.91f7468f.chunk.css"
+  },
+  {
+    "revision": "8da08b4992cf51eb01fb",
+    "url": "/newsie/static/js/2.b101d06a.chunk.js"
+  },
+  {
+    "revision": "d0716139e3036c6c4d38",
+    "url": "/newsie/static/js/main.3854be64.chunk.js"
+  },
+  {
+    "revision": "76618213e180780999eb",
+    "url": "/newsie/static/js/runtime~main.dd8e392e.js"
+  },
+  {
+    "revision": "3360ee53634611c7635881f4e8948d2c",
+    "url": "/newsie/static/media/CNBC.3360ee53.png"
+  },
+  {
+    "revision": "73fa86d998579d9401ea2f99819f6fc5",
+    "url": "/newsie/static/media/CNBC.73fa86d9.png"
+  },
+  {
+    "revision": "cb8069ce738500c7d1304294325fc2b4",
+    "url": "/newsie/static/media/Fortune.cb8069ce.png"
+  },
+  {
+    "revision": "dfa7c29b246d2311395755523d1a2bac",
+    "url": "/newsie/static/media/Fortune.dfa7c29b.png"
+  },
+  {
+    "revision": "f2dc8f597f7763a53692b59c72a2afe9",
+    "url": "/newsie/static/media/The Economist.f2dc8f59.png"
+  },
+  {
+    "revision": "a6395eddbeff501b0b0efae5e05cf83f",
+    "url": "/newsie/static/media/bloomberg.a6395edd.png"
+  },
+  {
+    "revision": "dff6bbc9449b7654b5f0f3165c9a1ab7",
+    "url": "/newsie/static/media/bloomberg.dff6bbc9.png"
+  },
+  {
+    "revision": "c664820f90643856d894ec879f436019",
+    "url": "/newsie/static/media/business-insider.c664820f.png"
+  },
+  {
+    "revision": "e4a4cdf7a442c47c5843770962b7ca78",
+    "url": "/newsie/static/media/business-insider.e4a4cdf7.png"
+  },
+  {
+    "revision": "95ff831499fd86c36eef36304405cb35",
+    "url": "/newsie/static/media/desktopImage.95ff8314.png"
+  },
+  {
+    "revision": "a156d9edcb4c6ebbbc4d3b9c70d70ac7",
+    "url": "/newsie/static/media/economist.a156d9ed.png"
+  },
+  {
+    "revision": "133390a18e6ece404cac575268ac578d",
+    "url": "/newsie/static/media/the-wall-street-journal.133390a1.png"
+  },
+  {
+    "revision": "538ef1c6fb5bb68ceee2b0f8edf2a682",
+    "url": "/newsie/static/media/the-wall-street-journal.538ef1c6.png"
+  }
+]);
