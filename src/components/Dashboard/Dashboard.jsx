58936d3a0f5a01b45f0718abbed3367317b5e8c9
Loriana Hines <lorianahines@gmail.com>
2019-05-22 22:46:52 -0400

    styled trending article

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -29,7 +29,7 @@ class Dashboard extends Component {
                   <h1>{article.title}</h1>
                   <h4>{moment(article.publishedAt).format('MM/DD HH:mm A')}</h4>
                   <p>{article.description}</p>
-                  <a href={article.url}>READ MORE</a>
+                  <a href={article.url} target="_blank">READ MORE</a>
                 </div>
               </div>
     }) : () => <h1>Articles loading...</h1> 
